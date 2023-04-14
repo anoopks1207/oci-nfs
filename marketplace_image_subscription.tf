bca5f9aee19b44059a36e3bb617bb1c16514012c
anoopks1207 <anoopks1207@gmail.com>
2023-04-14 23:17:36 +0530

    Update marketplace_image_subscription.tf

--- marketplace_image_subscription.tf
+++ marketplace_image_subscription.tf
@@ -1,7 +1,7 @@
 #Local variables pointing to the Marketplace catalog resource
 #Eg. Modify accordingly to your Application/Listing
 locals {
-  mp_listing_id = var.use_marketplace_image ? var.marketplace_listing_id : substr(var.marketplace_listing,0,3) 
+  mp_listing_id = var.use_marketplace_image ? var.mp_listing_id : substr(var.marketplace_listing,0,3) 
   #mp_listing_id               = var.mp_listing_id
   mp_listing_resource_id      = var.mp_listing_resource_id
   mp_listing_resource_version = var.mp_listing_resource_version
