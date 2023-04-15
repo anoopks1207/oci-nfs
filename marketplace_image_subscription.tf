d4f7cc08224ff7346f82bc8a4d2e320e99aea5e6
anoopks1207 <anoopks1207@gmail.com>
2023-04-15 17:43:37 +0530

    Update marketplace_image_subscription.tf

--- marketplace_image_subscription.tf
+++ marketplace_image_subscription.tf
@@ -51,7 +51,7 @@ data "oci_core_app_catalog_subscriptions" "mp_image_subscription" {
 
 ############
 #Get Image Agreement
-resource "oci_core_app_catalog_listing_resource_version_agreement" "mp_image_agreement" {
+resource "oci_core_app_catalog_listing_resource_version_agreement" "mp_image_agreement_ol8" {
   count = var.use_marketplace_image ? 1 : 0
 
   listing_id               = local.mp_listing_id_ol8
@@ -62,12 +62,12 @@ resource "oci_core_app_catalog_listing_resource_version_agreement" "mp_image_agr
 resource "oci_core_app_catalog_subscription" "mp_image_subscription" {
   count                    = var.use_marketplace_image ? 1 : 0
   compartment_id           = var.compartment_ocid
-  eula_link                = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].eula_link
-  listing_id               = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].listing_id
-  listing_resource_version = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].listing_resource_version
-  oracle_terms_of_use_link = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].oracle_terms_of_use_link
-  signature                = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].signature
-  time_retrieved           = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].time_retrieved
+  eula_link                = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement_ol8[0].eula_link
+  listing_id               = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement_ol8[0].listing_id
+  listing_resource_version = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement_ol8[0].listing_resource_version
+  oracle_terms_of_use_link = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement_ol8[0].oracle_terms_of_use_link
+  signature                = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement_ol8[0].signature
+  time_retrieved           = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement_ol8[0].time_retrieved
 
   timeouts {
     create = "20m"
