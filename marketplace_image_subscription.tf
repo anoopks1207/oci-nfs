0d0d7ff012ff864ac826eae5830d53d5c1813f21
anoopks1207 <anoopks1207@gmail.com>
2023-04-15 17:47:17 +0530

    Update marketplace_image_subscription.tf

--- marketplace_image_subscription.tf
+++ marketplace_image_subscription.tf
@@ -59,7 +59,7 @@ resource "oci_core_app_catalog_listing_resource_version_agreement" "mp_image_agr
 }
 
 #Accept Terms and Subscribe to the image, placing the image in a particular compartment
-resource "oci_core_app_catalog_subscription" "mp_image_subscription" {
+resource "oci_core_app_catalog_subscription" "mp_image_subscription_ol8" {
   count                    = var.use_marketplace_image ? 1 : 0
   compartment_id           = var.compartment_ocid
   eula_link                = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement_ol8[0].eula_link
@@ -75,7 +75,7 @@ resource "oci_core_app_catalog_subscription" "mp_image_subscription" {
 }
 
 # Gets the partner image subscription
-data "oci_core_app_catalog_subscriptions" "mp_image_subscription" {
+data "oci_core_app_catalog_subscriptions" "mp_image_subscription_ol8" {
   #Required
   compartment_id = var.compartment_ocid
 
