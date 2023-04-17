ba46b48aefc265e01b204eda041b3d786ca885b3
anoopks1207 <anoopks1207@gmail.com>
2023-04-17 09:16:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -260,8 +260,8 @@ variable mp_listing_resource_version { default = "1.0" }
 variable use_marketplace_image { default = true }
 
 # ------------------------------------------------------------------------------------------------------------
-variable image_id {  default = "" }
-variable "mp_listing_resource_id" {}
+variable image_id { default = "" }
+variable "mp_listing_resource_id" { default = "" }
 variable marketplace_listing {  default = "" }
 variable marketplace_listing_id_ol7 { default = "ocid1.appcataloglisting.oc1..aaaaaaaa26y5fkfvbjmspmuuhpoi6jptq3gc635a3gz72qujfsomvczh2miq" }  #ol7
 variable marketplace_listing_id_ol8 { default = "ocid1.image.oc1.ap-mumbai-1.aaaaaaaabuerxav5rirhzbtexyamgkuawq243s462irkgzvq6lq6giziu3vq" } #ol8
